152d379d74e9776714c65c1146162dd328adb101
E-Vargo <vargoelliott@gmail.com>
2022-03-14 11:29:47 -0700

    Done.

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,2 +1,11 @@
 module ArtistsHelper
+    
+    def display_artist(song)
+        if song.artist && song.artist.name != ""
+            a = song.artist
+            link_to "#{a.name}", artist_path(a)
+        else
+            link_to "Add Artist", edit_song_path(song)
+        end
+    end
 end
